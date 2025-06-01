00c320023e47741dc51f6e471f950b833db8572c
codershahid <codershahid@icloud.com>
2025-06-01 18:24:01 +0530

    implemented provision to view temp in different units

--- src/components/dash/Dash.jsx
+++ src/components/dash/Dash.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 const Dash = ({ weatherData }) => {
+  const [unit, setUnit] = useState("celsius");
+
+  const getTemp = (temp, unit) => {
+    return unit === "celsius"
+      ? Math.round(temp - 273.15) + "°C"
+      : Math.round((temp * 9) / 5 - 459.67) + "°F";
+  };
   return (
     <div>
-      <h1>Dash Component</h1>
-      <h3>The weather today is {weatherData.main.temp}</h3>
+      <h1>{weatherData.name}</h1>
+      <p>
+        <label htmlFor="unitType">View in Fahrenheit</label>{" "}
+        <input
+          type="checkbox"
+          id="unitType"
+          onChange={(value) =>
+            setUnit(value.target.checked ? "fahrenheit" : "celsius")
+          }
+        />
+      </p>
+      <p>
+        <span>Temp: {getTemp(weatherData.main.temp, unit)}</span>{" "}
+      </p>
+      <span>Description: {weatherData.weather[0].description}</span>
     </div>
   );
 };
