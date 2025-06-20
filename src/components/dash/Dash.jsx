50c06d928cd34ae292bb2b8d879502972aeeeeb3
arslaanqayoom <arslaanq007@gmail.com>
2025-06-20 17:56:34 +0530

    included more details

--- src/components/dash/Dash.jsx
+++ src/components/dash/Dash.jsx
@@ -11,6 +11,12 @@ const Dash = ({ weatherData }) => {
   return (
     <div>
       <h1>{weatherData.name}</h1>
+      <h2>
+        {" "}
+        {weatherData.coord.lon}
+        {" , "}
+        {weatherData.coord.lat}
+      </h2>
       <p>
         <label htmlFor="unitType">View in Fahrenheit</label>{" "}
         <input
@@ -21,10 +27,23 @@ const Dash = ({ weatherData }) => {
           }
         />
       </p>
+      <h2>
+        <span>Description: {weatherData.weather[0].description}</span>
+      </h2>
       <p>
         <span>Temp: {getTemp(weatherData.main.temp, unit)}</span>{" "}
       </p>
-      <span>Description: {weatherData.weather[0].description}</span>
+      <p>
+        <span>Feels like: {getTemp(weatherData.main.feels_like, unit)}</span>{" "}
+      </p>
+      <p>
+        <span>Pressure: {weatherData.main.pressure}</span>
+        {"mb"}
+      </p>
+      <p>
+        <span>Humidity: {weatherData.main.humidity}</span>
+        {"%"}
+      </p>
     </div>
   );
 };
